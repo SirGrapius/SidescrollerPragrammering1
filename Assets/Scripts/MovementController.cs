bc6777076b9b2f372c17b20075aacc57a7bc4df0
Adonai <adonai.osterberg@elev.ga.lbs.se>
2023-09-29 10:44:17 +0200

    Unfinished Jumping

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    
-    void Start()
+    public enum CharacterState
     {
-        
+        Grounded = 0,
+        Airborne = 1,
+        Jumping = 2,
+        Total
     }
 
+    public CharacterState JumpingState = CharacterState.Airborne;
+
+    //Gravity
     public float GravityPerSecond = 160.0f; //falling speed
-    public float GroundLevel = 0.0f; //Ground Value
+    public float GroundLevel = 0.0f; //Ground
+    //Jump
+    public float JumpSpeedFactor = 3.0f;
+    public float JumpMaxHeight = 150.0f;
+    private float JumpHeightDelta = 0.0f;
+    //Movement
     public float MovementSpeedPerSecond = 10.0f; //Movement Speed
 
 
@@ -23,13 +33,36 @@ public class MovementController : MonoBehaviour
         if (gravityPosition.y < GroundLevel) { gravityPosition.y = GroundLevel; } //Set character to ground level
         transform.position = gravityPosition; // Assaign new pos to transform
 
-        //Up
-        if (Input.GetKey(KeyCode.W))
+        //1. Check if player is on the ground so they can jump
+        if(transform.position.y <= GroundLevel)
         {
-            Vector3 characterPosition = transform.position; //copy char position
-            characterPosition.y += MovementSpeedPerSecond * Time.deltaTime; //add movementspeed * time per frame
-            transform.position = characterPosition; //assign new position
+            Vector3 characterPosition = transform.position;
+            characterPosition.y = GroundLevel;
+            transform.position = characterPosition;
+            JumpingState = CharacterState.Grounded;
         }
+        //Up
+        //2. Check if player presses the jump button
+        if (Input.GetKey(KeyCode.W) && JumpingState == CharacterState.Grounded)
+        {
+            JumpingState = CharacterState.Jumping; //Set character to jumping
+            JumpHeightDelta = 0.0f; //Restart counting jumpdistance
+        }
+        //3. Do the jump math
+        if (JumpingState == CharacterState.Jumping)
+        {
+            Vector3 characterPosition = transform.position;
+            float totalJumpMovementThisFrame += MovementSpeedPerSecond * JumpSpeedFactor * Time.deltaTime;
+            characterPosition.y += totalJumpMovementThisFrame;
+            transform.position = characterPosition;
+            JumpHeightDelta += totalJumpMovementThisFrame;
+            if(JumpHeightDelta >= JumpMaxHeight)
+            {
+                JumpingState = CharacterState.Airborne;
+                JumpHeightDelta = 0.0f;
+            }
+        }
+
         //Left
         if(Input.GetKey(KeyCode.A))
         {
@@ -37,13 +70,15 @@ public class MovementController : MonoBehaviour
             characterPosition.x -= MovementSpeedPerSecond * Time.deltaTime; //add movementspeed * time per frame
             transform.position = characterPosition; //assign new position
         }
+
         //Down
         if(Input.GetKey(KeyCode.S))
         {
             Vector3 characterPosition = transform.position; //copy char position
             characterPosition.y -= MovementSpeedPerSecond * Time.deltaTime; //add movementspeed * time per frame
             transform.position = characterPosition; //assign new position
-        }
+        
+
         //Right
         if(Input.GetKey(KeyCode.D))
         {
